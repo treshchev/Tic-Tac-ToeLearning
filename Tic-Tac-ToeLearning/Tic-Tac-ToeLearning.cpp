8a071918bfbcb794327432cde4f391bd09763466
Vladislav Treshchev <letsgovlad@gmail.com>
2024-06-27 10:45:21 +0300

    Created the game TIc Tac Toe

--- Tic-Tac-ToeLearning/Tic-Tac-ToeLearning.cpp
+++ Tic-Tac-ToeLearning/Tic-Tac-ToeLearning.cpp
@@ -1,8 +1,37 @@
 //Copyright Vladislav Treshchev. All rights had been reserved
-#include <iostream>
+// Program Tic-Tac-Toe Board
+// Describes some actions with multidimensional arrays
 
+#include <iostream>
 int main()
 {
-    std::cout << "Vladislav Treshchev\n";
-}
+	const int ROWS = 3;
+	const int COLUMS = 3;
 
+	char board[ROWS][COLUMS] = { {'O', 'X', 'O'},
+		{' ', 'X', 'X'},
+		{'X', 'O', 'O'} };
+
+	std::cout << "Here's the tic-tac-toe board:\n";
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLUMS; j++)
+		{
+			std::cout << board[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "\n'X' moves to the empty location.\n\n";
+	board[1][0] = 'X';
+	std::cout << "Now the tic-tac-toe board is:\n";
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLUMS; j++)
+		{
+			std::cout << board[i][j];
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "\n'X' wins!!!";
+	return 0;
+}
